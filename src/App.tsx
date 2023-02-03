090196895ea80f7a0dd8a0eac6392e2a76485c25
naniitevez <nicole.tevez.97@gmail.com>
2023-02-03 19:34:02 -0300

    agrego carousel al hero section

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import "./App.css";
+import "./styles/App.scss";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Navbar from "./components/navbar/Navbar";
