9133ea6b8d31f1ab81d06193e7ed843818a71e21
naniitevez <nicole.tevez.97@gmail.com>
2023-02-06 18:03:15 -0300

    agrego componente de loader

---
+++ src/components/LoaderComponent.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const LoaderComponent = () => {
+  return (
+    <div id="loader-container">
+      <span className="loader"></span>
+    </div>
+  );
+};
+
+export default LoaderComponent;
